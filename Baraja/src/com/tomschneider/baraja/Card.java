5c4b6685ad85f10fd8b99b797403e48d8cbdc47b
Thomas Alcala <thomas.alcala@gmail.com>
2014-12-02 21:30:50 -0600

    Init the card dealing

---
+++ Baraja/src/com/tomschneider/baraja/Card.java
@@ -0,0 +1,43 @@
+package com.tomschneider.baraja;
+
+import android.graphics.Bitmap;
+
+public class Card {
+	/*
+	 * 101 - 110: Oros
+	 * 201 - 210: Bastos
+	 * 301 - 310: Espadas
+	 * 401 - 410: Copas
+	 */
+	private int id;
+	private Bitmap bmp;
+	
+	private int suit;
+	private int rank;
+	
+	public Card(int newId) {
+		id = newId;
+		suit = id - Integer.parseInt(String.valueOf(id).substring(1));
+		rank = id - suit;
+	}
+	
+	public void setBitmap(Bitmap tmpBmp) {
+		bmp = tmpBmp;
+	}
+	
+	public Bitmap getBitmap() {
+		return bmp;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getSuit() {
+		return suit;
+	}
+	
+	public int getRank() {
+		return rank;
+	}
+}
--- Baraja/src/com/tomschneider/baraja/GameView.java
+++ Baraja/src/com/tomschneider/baraja/GameView.java
@@ -1,8 +1,16 @@
 package com.tomschneider.baraja;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class GameView extends View {
@@ -10,6 +18,20 @@ public class GameView extends View {
 	
 	private float scale;
 	private int screenWidth, screenHeight;
+	
+	private ArrayList<Card> deck = new ArrayList<Card>();
+	private ArrayList<Card> discardPile = new ArrayList<Card>();
+	private ArrayList<Card> hand = new ArrayList<Card>();
+	
+	private int initCardsNumber;
+	
+	private int movingCardIdx = -1;
+	private int movingX;
+	private int movingY;
+	
+	private int scaledCardW, scaledCardH;
+	
+	Paint mPaint;
 
 	public GameView(Context context) {
 		super(context);
@@ -18,6 +40,8 @@ public class GameView extends View {
 		mContext = context;
 		
 		scale = mContext.getResources().getDisplayMetrics().density;
+		
+		initCardsNumber = 8;
 	}
 	
 	@Override
@@ -25,11 +49,54 @@ public class GameView extends View {
         super.onSizeChanged(w, h, oldW, oldH);
         screenWidth = w;
         screenHeight = h;
+        
+        initCards();
+        dealCards();
     }
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		canvas.drawColor(Color.DKGRAY);
+		mPaint = new Paint();
+		
+		for (int i = 0; i < hand.size(); i++) {
+			canvas.drawBitmap(hand.get(i).getBitmap(), (float) (i * (scaledCardW + 5)), (float) (screenHeight - scaledCardH - 15), mPaint);
+		}
+		
+		invalidate();
+	}
+	
+	private void initCards() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 101; j < 111; j++) {
+				int tempId = j + (i * 100);
+				Card tempCard = new Card(tempId);
+				int resourceId = getResources().getIdentifier("card_" + tempId, "drawable", mContext.getPackageName());
+				Bitmap tempBitmap = BitmapFactory.decodeResource(mContext.getResources(), resourceId);
+				scaledCardW = (int) (screenWidth / 10);
+				scaledCardH = (int) (scaledCardW * 1.28);
+				Bitmap scaledBitmap = Bitmap.createScaledBitmap(tempBitmap, scaledCardW, scaledCardH, false);
+				tempCard.setBitmap(scaledBitmap);
+				deck.add(tempCard);
+			}
+		}
+	}
+	
+	private boolean drawCard(List<Card> handToDraw) {
+		if (deck.size() > 0) {
+			handToDraw.add(0, deck.get(0));
+			deck.remove(0);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private void dealCards() {
+		Collections.shuffle(deck, new Random());
+		for (int i = 0; i < initCardsNumber; i++) {
+			drawCard(hand);
+		}
 	}
 
 }
